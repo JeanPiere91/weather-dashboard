b97a8c2f8e5e474b3306d1f11586d42b2e03fcbd
Jean Piere Bellota Mendoza <jeanpiere.bellota@gmail.com>
2024-01-08 01:47:53 +1100

    function to normalize data response from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+function normalizeData(data){
+    /* Get hour to get information for the next days*/
+    var hour = dayjs(data.list[0].dt_txt).format('HH')
+
+    /* Array information weather for current City */
+    var information = [];
+    for (var i = 0; i < data.list.length; i++) {
+        if(dayjs(data.list[i].dt_txt).format('HH') == hour){
+            var weather = {
+                date: dayjs(data.list[i].dt_txt).format('MM/DD/YY'),
+                ico: 'https://openweathermap.org/img/wn/'+ data.list[i].weather[0].icon+'@2x.png',
+                temp: convertToDegreesFahrenheit(data.list[i].main.temp) + ' °F', //'76.62°F',
+                wind: data.list[i].wind.speed + ' MPH', //'8.43 MPH',
+                humidity: data.list[i].main.humidity + ' %', //'44 %',
+            }
+            information.push(weather)
+        }
+    }
+
+    var weatherCity = {
+        name: data.city.name,
+        information: information
+    }
+
+    return weatherCity;
+    
+}
+
 function searchApi(query) {
     
     var locQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ query + '&appid=' + APIKey;
